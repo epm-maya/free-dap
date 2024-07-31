24229c0ac39e8850a598a1a5d3a62e1f32ce205d
Mukai Tetsuji <mukait@gmail.com>
2024-07-31 16:34:40 +0900

    Add XIAO

--- platform/samd21/hal_config.h
+++ platform/samd21/hal_config.h
@@ -11,7 +11,47 @@
 /*- Definitions -------------------------------------------------------------*/
 #define HAL_BOARD_GENERIC
 
-#if defined(HAL_BOARD_CUSTOM)
+#ifdef XIAO_PINOUT
+/* XIAO
+
+A0 A02 | 5V
+A1 A04 | GND
+A2 A10 | 3V3
+A3 A11 | PA6 SWCLK
+D4 PA8 | PA5 SWDIO
+D5 PA9 | PA7 nRESET
+TX PB8 | PB9 RX
+
+PA18 RX_LED (DAP_STATUS)
+PA19 TX_LED (VCP_STATUS)
+*/
+
+  #define HAL_CONFIG_ENABLE_VCP
+  #define DAP_CONFIG_ENABLE_JTAG
+
+  HAL_GPIO_PIN(SWCLK_TCK,          A, 6)
+  HAL_GPIO_PIN(SWDIO_TMS,          A, 5)
+  HAL_GPIO_PIN(TDI,                A, 10)
+  HAL_GPIO_PIN(TDO,                A, 11)
+  HAL_GPIO_PIN(nRESET,             A, 7)
+
+  HAL_GPIO_PIN(VCP_STATUS,         A, 19);
+  HAL_GPIO_PIN(DAP_STATUS,         A, 18);
+  HAL_GPIO_PIN(BOOT_ENTER,         A, 2);
+
+  HAL_GPIO_PIN(UART_TX,            B, 8);
+  HAL_GPIO_PIN(UART_RX,            B, 9);
+
+  #define UART_SERCOM              SERCOM4
+  #define UART_SERCOM_PMUX         PORT_PMUX_PMUXE_D_Val
+  #define UART_SERCOM_GCLK_ID      SERCOM4_GCLK_ID_CORE
+  #define UART_SERCOM_APBCMASK     PM_APBCMASK_SERCOM4
+  #define UART_SERCOM_IRQ_INDEX    SERCOM4_IRQn
+  #define UART_SERCOM_IRQ_HANDLER  irq_handler_sercom4
+  #define UART_SERCOM_TXPO         0 // PAD[0]
+  #define UART_SERCOM_RXPO         1 // PAD[1]
+
+#elif defined(HAL_BOARD_CUSTOM)
   // Externally supplied board configuration takes precedence
   #include HAL_BOARD_CUSTOM
 
